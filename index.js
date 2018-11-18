7dabd52f37f218ef6050093408271da1aa062207
Michael Checo <checomichael2@gmail.com>
2018-11-18 07:55:42 -0500

    server running + knex config

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile')
+const db = knex(knexConfig.development)
+const server = express()
+server.use(express.json())
+server.get('/', (req, res) => {
+    res.send('hi')
+})
+const port = 9000;
+server.listen(port, () => console.log(`==^_^== ${port} ==^_^==`))
